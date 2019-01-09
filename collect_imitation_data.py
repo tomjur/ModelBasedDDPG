1da458452dc1d1bb40877ced0014fe06275899fd
tom <tomjurgenson@gmail.com>
2019-01-09 18:23:54 +0200

    collect imitation data fix

--- collect_imitation_data.py
+++ collect_imitation_data.py
@@ -8,7 +8,7 @@ from data_collector import CollectorProcess, DataCollector
 
 
 class TrajectoryCollectorProcess(CollectorProcess):
-    def _get_tuple(self):
+    def _get_tuple(self, query_params=None):
         start_joints, goal_joints, _, trajectory = self.openrave_interface.start_new_random(None, return_traj=True)
         trajectory_poses = [
             self.openrave_interface.openrave_manager.get_potential_points_poses(step) for step in trajectory]
@@ -19,9 +19,9 @@ class ImitationDataCollector(DataCollector):
     def _get_queue_size(self, number_of_threads):
         return 2 * number_of_threads
 
-    def _get_collector(self, config, queued_data_points, result_queue, collector_specific_queue, params_file=None):
+    def _get_collector(self, config, queued_data_points, collector_specific_queue, params_file=None):
         return TrajectoryCollectorProcess(
-            config, queued_data_points, result_queue, collector_specific_queue, params_file)
+            config, queued_data_points, self.results_queue, collector_specific_queue, params_file)
 
 
 # read the config
@@ -32,19 +32,20 @@ with open(config_path, 'r') as yml_file:
     print(yaml.dump(config))
 
 config['openrave_rl']['challenging_trajectories_only'] = True
-# params_file = None
-# params_file = 'params_simple.pkl'
-params_file = 'params_hard.pkl'
+# scenario = 'simple'
+scenario = 'hard'
+params_file = os.path.abspath(os.path.expanduser(
+    os.path.join('~/ModelBasedDDPG/scenario_params', scenario, 'params.pkl')))
 
-number_of_trajectories = 8
-trajectories_per_file = 4
-threads = 2
-results_dir = 'imitation_data_to_delete'
+# number_of_trajectories = 8
+# trajectories_per_file = 4
+# threads = 1
+# results_dir = 'imitation_data_to_delete'
 
-# number_of_trajectories = 100000
-# trajectories_per_file = 1000
-# threads = 100
-# results_dir = 'imitation_data'
+number_of_trajectories = 100000
+trajectories_per_file = 1000
+threads = 100
+results_dir = 'imitation_data'
 
 data_collector = ImitationDataCollector(config, threads, params_file)
 if not os.path.exists(results_dir):
