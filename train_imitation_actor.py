ed21f3661e80beb4144866cd7e2bc4922ae0ac32
tom <tomjurgenson@gmail.com>
2019-01-16 15:50:04 +0200

    imitation learning with caching

--- train_imitation_actor.py
+++ train_imitation_actor.py
@@ -11,87 +11,9 @@ import tensorflow as tf
 import multiprocessing
 
 from network import Network
+from potential_point import PotentialPoint
 from rollout_manager import ActorProcess
 from trajectory_eval import TrajectoryEval
-
-
-class ImitationRolloutManager:
-    def __init__(self, config, train_trajectories, test_trajectories):
-        self._train_index = 0
-        self._test_index = 0
-        self.train_trajectories = train_trajectories
-        self.test_trajectories = test_trajectories
-
-        self.episode_generation_queue = multiprocessing.JoinableQueue()
-        self.episode_results_queue = multiprocessing.Queue()
-
-        actor_processes = config['general']['actor_processes']
-        if actor_processes is None:
-            actor_processes = multiprocessing.cpu_count()
-
-        self.actor_specific_queues = [multiprocessing.JoinableQueue() for _ in range(actor_processes)]
-
-        self.actors = [
-            ActorProcess(copy.deepcopy(config), self.episode_generation_queue, self.episode_results_queue,
-                         self.actor_specific_queues[i], None)
-            for i in range(actor_processes)
-        ]
-        # start all the actor processes
-        for a in self.actors:
-            a.start()
-        # for every actor process, post a message to initialize the actor network
-        for actor_queue in self.actor_specific_queues:
-            actor_queue.put((0, ))
-            actor_queue.join()
-
-    def generate_episodes(self, number_of_episodes, is_train):
-        # use collectors to generate queries
-        for i in range(number_of_episodes):
-            if is_train:
-                traj = self.train_trajectories[self._train_index]
-                self._train_index += 1
-                if self._train_index == len(self.train_trajectories):
-                    self._train_index = 0
-                    random.shuffle(self.train_trajectories)
-            else:
-                traj = self.test_trajectories[self._test_index]
-                self._test_index += 1
-                if self._test_index == len(self.test_trajectories):
-                    self._test_index = 0
-                    random.shuffle(self.test_trajectories)
-            # get a query
-            message = ((traj, None, None), False)  # poses and workspace image are not required
-            # message = ((traj, None, None), True)  # poses and workspace image are not required
-            # place in queue
-            self.episode_generation_queue.put(message)
-
-        self.episode_generation_queue.join()
-
-        episodes = []
-        while number_of_episodes:
-            number_of_episodes -= 1
-            episodes.append(self.episode_results_queue.get())
-
-        return episodes
-
-    def set_policy_weights(self, weights, is_online):
-        message = (2, weights, is_online)
-        self._post_private_message(message, self.actor_specific_queues)
-
-    def end(self):
-        message = (1, )
-        self._post_private_message(message, self.actor_specific_queues)
-        time.sleep(10)
-        for a in self.actors:
-            a.terminate()
-        time.sleep(10)
-
-    @staticmethod
-    def _post_private_message(message, queues):
-        for queue in queues:
-            queue.put(message)
-        for queue in queues:
-            queue.join()
 
 
 model_name = datetime.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d_%H_%M_%S')
@@ -115,50 +37,81 @@ config['general']['params_file'] = os.path.abspath(
 test_batch_size = batch_size * 10
 
 
-def load_data_from(data_dir, max_read=None):
-    print 'loading from {} max_read {}'.format(data_dir, max_read)
+def produce_transitions(data_dir, cache_dir):
+    print 'producing transition data from original trajectories at {}'.format(data_dir)
     assert os.path.exists(data_dir)
+
+
+    if os.path.exists(cache_dir):
+        print 'found cache dir at {}, assuming all transitions are present there (if not delete the directory)'.format(
+            cache_dir)
+        return
+
+    print 'cache not found, creating cache at: {}'.format(cache_dir)
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
     files = [file for file in os.listdir(data_dir) if file.endswith(".path_pkl")]
     assert len(files) > 0
-    random.shuffle(files)
-    paths = []
+    target_point = PotentialPoint.from_config(config)[-1]
     for file in files:
-        if max_read is not None and len(paths) >= max_read:
-            break
+        print 'loading file {}'.format(file)
         with bz2.BZ2File(os.path.join(data_dir, file), 'r') as compressed_file:
-            current_buffer = pickle.load(compressed_file)
-            paths.extend(current_buffer)
+            paths = pickle.load(compressed_file)
 
-    # take only the required size
-    if max_read is not None:
-        paths = paths[:max_read]
+        print 'asserting step sizes match'
+        step_size = config['openrave_rl']['action_step_size'] + 0.00001
+        for (traj, _) in paths:
+            for i in range(len(traj) - 1):
+                assert np.linalg.norm(np.array(traj[i]) - np.array(traj[i + 1])) < step_size
 
-    # assert length
-    step_size = config['openrave_rl']['action_step_size'] + 0.00001
-    for (traj, _) in paths:
-        for i in range(len(traj)-1):
-            assert np.linalg.norm(np.array(traj[i]) - np.array(traj[i+1])) < step_size
+        print 'creating transitions'
+        transitions = []
+        for (traj, poses_trajectory) in paths:
+            goal_joints = traj[-1]
+            goal_pose = poses_trajectory[-1][target_point.tuple]
+            for i in range(len(traj) - 1):
+                joints = traj[i]
+                next_joints = traj[i + 1]
+                transition = (joints[1:], next_joints[1:], goal_joints[1:], goal_pose)
+                transitions.append(transition)
 
-    # partition traj to buffer (current joints, goal, next_joints)
-    target_point = tuple(config['model']['potential_points'])
-    transitions = []
-    for (traj, poses_trajectory) in paths:
-        goal_joints = traj[-1]
-        goal_pose = poses_trajectory[-1][target_point]
-        for i in range(len(traj)-1):
-            joints = traj[i]
-            next_joints = traj[i+1]
-            transition = (joints[1:], next_joints[1:], goal_joints[1:], goal_pose)
-            transitions.append(transition)
-    paths = [p[0] for p in paths]
-    print 'done reading from {}'.format(data_dir)
-    return paths, transitions
+        transition_file = os.path.join(cache_dir, file + '.transitions_cache')
+        print 'writing transitions file {}'.format(transition_file)
+        with bz2.BZ2File(transition_file, 'w') as compressed_file:
+            pickle.dump(transitions, compressed_file)
+
+    print 'cache created at {}'.format(cache_dir)
 
 
-train_paths, train_transitions = load_data_from(
-    os.path.join('imitation_data', scenario, 'train'), config['general']['train_episodes_dataset'])
-test_paths, test_transitions = load_data_from(
-    os.path.join('imitation_data', scenario, 'test'), config['general']['test_episodes_dataset'])
+train_paths_dir = os.path.join('imitation_data', scenario, 'train')
+train_transitions_dir = os.path.join('imitation_data_transitions', scenario, 'train')
+train_transitions_dir = os.path.join(train_transitions_dir, PotentialPoint.from_config(config)[-1].str)
+produce_transitions(train_paths_dir, train_transitions_dir)
+test_paths_dir = os.path.join('imitation_data', scenario, 'test')
+test_transitions_dir = os.path.join('imitation_data_transitions', scenario, 'test')
+test_transitions_dir = os.path.join(test_transitions_dir, PotentialPoint.from_config(config)[-1].str)
+produce_transitions(test_paths_dir, test_transitions_dir)
+
+
+def get_files(paths_dir, transitions_dir, max_files=None):
+    print 'loading from paths {} transitions {}. max files {}'.format(paths_dir, transitions_dir, max_files)
+    assert os.path.exists(paths_dir)
+    assert os.path.exists(transitions_dir)
+    files = [file for file in os.listdir(paths_dir) if file.endswith(".path_pkl")]
+    assert len(files) > 0
+    for file in files:
+        assert os.path.exists(os.path.join(transitions_dir, file + '.transitions_cache'))
+    random.shuffle(files)
+    files = files[:max_files]
+    path_files = [os.path.join(paths_dir, f) for f in files]
+    transition_files = [os.path.join(transitions_dir, f + '.transitions_cache') for f in files]
+    return path_files, transition_files
+
+
+train_path_files, train_transition_files = get_files(train_paths_dir, train_transitions_dir, config[
+    'general']['train_files'])
+test_path_files, test_transition_files = get_files(test_paths_dir, test_transitions_dir, config[
+    'general']['test_files'])
 
 # set summaries and saver dir
 base_dir = os.path.join('imitation', scenario)
@@ -226,11 +179,6 @@ test_rate_summary = tf.summary.merge([
 ])
 
 
-def get_batch(data_collection, data_index, batch_size):
-    batch = data_collection[data_index:data_index + batch_size]
-    return zip(*batch)
-
-
 def print_state(prefix, episodes, successful_episodes, collision_episodes, max_len_episodes):
     print '{}: {}: finished: {}, successful: {} ({}), collision: {} ({}), max length: {} ({})'.format(
         datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), prefix, episodes,
@@ -239,8 +187,96 @@ def print_state(prefix, episodes, successful_episodes, collision_episodes, max_l
     )
 
 
+class ImitationRolloutManager:
+    def __init__(self, config, train_trajectories_files, test_trajectories_files):
+        self._train_index = 0
+        self._test_index = 0
+        self.train_trajectories = self.files_to_trajectories(train_trajectories_files)
+        self.test_trajectories = self.files_to_trajectories(test_trajectories_files)
+
+        self.episode_generation_queue = multiprocessing.JoinableQueue()
+        self.episode_results_queue = multiprocessing.Queue()
+
+        actor_processes = config['general']['actor_processes']
+        if actor_processes is None:
+            actor_processes = multiprocessing.cpu_count()
+
+        self.actor_specific_queues = [multiprocessing.JoinableQueue() for _ in range(actor_processes)]
+
+        self.actors = [
+            ActorProcess(copy.deepcopy(config), self.episode_generation_queue, self.episode_results_queue,
+                         self.actor_specific_queues[i], None)
+            for i in range(actor_processes)
+        ]
+        # start all the actor processes
+        for a in self.actors:
+            a.start()
+        # for every actor process, post a message to initialize the actor network
+        for actor_queue in self.actor_specific_queues:
+            actor_queue.put((0, ))
+            actor_queue.join()
+
+    @staticmethod
+    def files_to_trajectories(files):
+        paths = []
+        for f in files:
+            with bz2.BZ2File(f, 'r') as compressed_file:
+                current_buffer = pickle.load(compressed_file)
+                paths.extend([t[0] for t in current_buffer])
+        return paths
+
+    def generate_episodes(self, number_of_episodes, is_train):
+        # use collectors to generate queries
+        for i in range(number_of_episodes):
+            if is_train:
+                traj = self.train_trajectories[self._train_index]
+                self._train_index += 1
+                if self._train_index == len(self.train_trajectories):
+                    self._train_index = 0
+                    random.shuffle(self.train_trajectories)
+            else:
+                traj = self.test_trajectories[self._test_index]
+                self._test_index += 1
+                if self._test_index == len(self.test_trajectories):
+                    self._test_index = 0
+                    random.shuffle(self.test_trajectories)
+            # get a query
+            message = ((traj, None, None), False)  # poses and workspace image are not required
+            # message = ((traj, None, None), True)  # poses and workspace image are not required
+            # place in queue
+            self.episode_generation_queue.put(message)
+
+        self.episode_generation_queue.join()
+
+        episodes = []
+        while number_of_episodes:
+            number_of_episodes -= 1
+            episodes.append(self.episode_results_queue.get())
+
+        return episodes
+
+    def set_policy_weights(self, weights, is_online):
+        message = (2, weights, is_online)
+        self._post_private_message(message, self.actor_specific_queues)
+
+    def end(self):
+        message = (1, )
+        self._post_private_message(message, self.actor_specific_queues)
+        time.sleep(10)
+        for a in self.actors:
+            a.terminate()
+        time.sleep(10)
+
+    @staticmethod
+    def _post_private_message(message, queues):
+        for queue in queues:
+            queue.put(message)
+        for queue in queues:
+            queue.join()
+
+
 # evaluate train paths
-rollout_manager = ImitationRolloutManager(config, train_paths, test_paths)
+rollout_manager = ImitationRolloutManager(config, train_path_files, test_path_files)
 train_trajectory_eval = TrajectoryEval(config, rollout_manager, train_completed_trajectories_dir)
 
 # evaluate test paths
@@ -256,6 +292,42 @@ latest_saver = tf.train.Saver(max_to_keep=2, save_relative_paths=saver_dir)
 best_saver = tf.train.Saver(max_to_keep=2, save_relative_paths=saver_dir)
 
 
+class TransitionDataLoader:
+    def __init__(self, files):
+        self.files = files
+
+    def __iter__(self):
+        random.shuffle(self.files)
+        for f in self.files:
+            with bz2.BZ2File(f, 'r') as compressed_file:
+                yield pickle.load(compressed_file)
+
+
+class Batcher:
+    def __init__(self, input_iterator, batch_size, shuffle_before_yield):
+        self.input_iterator = input_iterator
+        self.batch_size = batch_size
+        self.shuffle_before_yield = shuffle_before_yield
+
+    def __iter__(self):
+        current_batch = []
+        for tuple_list in self.input_iterator:
+            for t in tuple_list:
+                current_batch.append(t)
+                if len(current_batch) == self.batch_size:
+                    if self.shuffle_before_yield:
+                        random.shuffle(current_batch)
+                    yield current_batch
+                    current_batch = []
+        if self.shuffle_before_yield:
+            random.shuffle(current_batch)
+        yield current_batch
+
+
+train_batcher = Batcher(TransitionDataLoader(train_transition_files), batch_size, True)
+test_batcher = Batcher(TransitionDataLoader(test_transition_files), test_batch_size, True)
+
+
 with tf.Session(
         config=tf.ConfigProto(
             gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=config['general']['gpu_usage'])
@@ -265,11 +337,8 @@ with tf.Session(
     for epoch in range(epochs):
         # run train for one epoch
         print 'starting epoch {}'.format(epoch)
-        random.shuffle(train_transitions)
-        data_index = 0
-        while data_index < len(train_transitions):
-            train_batch = get_batch(train_transitions, data_index, batch_size)
-            data_index += batch_size
+        for raw_train_batch in train_batcher:
+            train_batch = zip(*raw_train_batch)
             train_feed = {
                 network.joints_inputs: train_batch[0],
                 next_joints_inputs: train_batch[1],
@@ -283,8 +352,9 @@ with tf.Session(
         train_summary_writer.flush()
 
         # run test for one (random) batch
-        random.shuffle(test_transitions)
-        test_batch = get_batch(test_transitions, 0, test_batch_size)
+        for raw_test_batch in test_batcher:
+            test_batch = zip(*raw_test_batch)
+            break
         test_feed = {
             network.joints_inputs: test_batch[0],
             next_joints_inputs: test_batch[1],
