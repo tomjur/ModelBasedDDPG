0635889c3bd7cdb07c1de939981d4b949a25d3ca
tom <tomjurgenson@gmail.com>
2019-02-21 10:43:30 +0200

    remove unused rollout classes

--- rollout_manager.py
+++ rollout_manager.py
@@ -14,42 +14,6 @@ from network import Network
 from openrave_rl_interface import OpenraveRLInterface
 
 
-class RandomQueryCollectorProcess(multiprocessing.Process):
-    def __init__(self, config, result_queue, collector_specific_queue):
-        multiprocessing.Process.__init__(self)
-        self.result_queue = result_queue
-        self.collector_specific_queue = collector_specific_queue
-        self.config = config
-        self.openrave_interface = None
-
-    def _run_main_loop(self):
-        episodes_per_update = self.config['general']['episodes_per_update']
-        required_trajectories = episodes_per_update * 10
-        while True:
-            try:
-                next_collector_specific_task = self.collector_specific_queue.get(block=True, timeout=0.1)
-                task_type = next_collector_specific_task[0]
-                # can only terminate
-                self.collector_specific_queue.task_done()
-                break
-            except Queue.Empty:
-                pass
-            if self.result_queue.qsize() < required_trajectories:
-                trajectory = self.openrave_interface.find_random_trajectory()
-                trajectory_poses = [
-                    self.openrave_interface.openrave_manager.get_potential_points_poses(step) for step in trajectory]
-                assert len(trajectory) == len(trajectory_poses)
-                result = trajectory, trajectory_poses
-                self.result_queue.put(result)
-
-    def run(self):
-        self.openrave_interface = OpenraveRLInterface(self.config)
-        params_file = self.config['general']['params_file']
-        if params_file is not None:
-            self.openrave_interface.openrave_manager.set_params(params_file)
-        self._run_main_loop()
-
-
 class FixedQueryCollectorProcess(multiprocessing.Process):
     def __init__(self, config, result_queue, collector_specific_queue, source_directory):
         multiprocessing.Process.__init__(self)
@@ -247,89 +211,6 @@ class ActorProcess(multiprocessing.Process):
         ) as sess:
             self._run_main_loop(sess)
 
-
-class RolloutManager:
-    def __init__(self, config, collector_processes=None, actor_processes=None, fixed_queries=None):
-        self.episode_generation_queue = multiprocessing.JoinableQueue()
-        self.episode_results_queue = multiprocessing.Queue()
-        self.query_results_queue = multiprocessing.Queue()
-        if collector_processes is None:
-            collector_processes = config['general']['collector_processes']
-        if actor_processes is None:
-            actor_processes = config['general']['actor_processes']
-        self.fixed_queries = fixed_queries
-
-        self.collector_specific_queue = [multiprocessing.JoinableQueue() for _ in range(collector_processes)]
-        self.actor_specific_queues = [multiprocessing.JoinableQueue() for _ in range(actor_processes)]
-
-        self.collectors = [
-            RandomQueryCollectorProcess(
-                copy.deepcopy(config), self.query_results_queue, self.collector_specific_queue[i]
-            )
-            for i in range(collector_processes)
-        ]
-        self.actors = [
-            ActorProcess(copy.deepcopy(config), self.episode_generation_queue, self.episode_results_queue,
-                         self.actor_specific_queues[i])
-            for i in range(actor_processes)
-        ]
-        # start all the collector processes
-        for c in self. collectors:
-            c.start()
-
-        # start all the actor processes
-        for a in self.actors:
-            a.start()
-        # for every actor process, post a message to initialize the actor network
-        for actor_queue in self.actor_specific_queues:
-            actor_queue.put((0, ))
-            actor_queue.join()
-
-    def generate_episodes(self, number_of_episodes, is_train):
-        if self.fixed_queries is None:
-            # use collectors to generate queries
-            for i in range(number_of_episodes):
-                # get a query
-                query = self.query_results_queue.get()
-                message = (query, is_train)
-                # place in queue
-                self.episode_generation_queue.put(message)
-        else:
-            #  use the fixed queries
-            random.shuffle(self.fixed_queries)
-            for (traj, image, goal_pose) in self.fixed_queries[:number_of_episodes]:
-                start_joints = traj[0]
-                goal_joints = traj[-1]
-                query = (traj, start_joints, goal_joints)
-                message = (query, is_train)
-                # place in queue
-                self.episode_generation_queue.put(message)
-
-        self.episode_generation_queue.join()
-
-        episodes = []
-        while number_of_episodes:
-            number_of_episodes -= 1
-            episodes.append(self.episode_results_queue.get())
-
-        return episodes
-
-    def set_policy_weights(self, weights):
-        message = (2, weights)
-        self._post_private_message(message, self.actor_specific_queues)
-
-    def end(self):
-        message = (1, )
-        self._post_private_message(message, self.actor_specific_queues)
-        self._post_private_message(message, self.collector_specific_queue)
-
-    @staticmethod
-    def _post_private_message(message, queues):
-        for queue in queues:
-            queue.put(message)
-        for queue in queues:
-            queue.join()
-            
 
 class FixedRolloutManager:
     def __init__(self, config, actor_processes=None, image_cache=None):
